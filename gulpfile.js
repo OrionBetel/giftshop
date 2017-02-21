c9e900b9ec0489b66dccd5adc503aecdbf9ff27e
Alexander Kharchenko <Alexander Kharchenko>
2017-02-21 14:01:00 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var htmlmin = require('gulp-htmlmin');
+ 
+gulp.task('sass', function() {
+  return gulp.src('src/styles/sass/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('src/styles/sass/*.scss', ['sass']);
+});
+
+gulp.task('html', function() {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function () {
+  gulp.watch(['src/styles/sass/*.scss', 'src/*.html'], ['sass', 'html']);
+});
